7cec37ea70716868973cdaa02633462b89b70929
John Weisz <johnmweisz@gmail.com>
2019-12-10 15:28:31 -0800

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Game
+    {
+        private static int answer;
+        public static void ChooseAnswer(int low, int high)
+        {
+            Random random = new Random();
+            answer = random.Next(low, high);
+        }
+
+        public static bool MakeGuess(int guess)
+        {
+
+        }
+    }
+
+}
