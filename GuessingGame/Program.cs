2cc815e24ff025266d8acc1f51e050e2605540e5
John Weisz <johnmweisz@gmail.com>
2019-12-10 15:43:36 -0800

    done

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,22 +10,34 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-
+            Game.ChooseAnswer(1, 11);
+            while (true)
+            {
+                Console.WriteLine(@"Make a guess or 'exit'");
+                var input = Console.ReadLine();
+                if (input == "exit") break;
+                if(Game.MakeGuess(Convert.ToInt32(input))) break;
+            }
+            Console.WriteLine("The answer was " + Game.ShowAnswer());
         }
     }
 
     public class Game
     {
-        private static int answer;
+        private static int _answer;
         public static void ChooseAnswer(int low, int high)
         {
-            Random random = new Random();
-            answer = random.Next(low, high);
+            var random = new Random();
+            _answer = random.Next(low, high);
         }
 
         public static bool MakeGuess(int guess)
         {
-
+            return guess == _answer;
+        }
+        public static int ShowAnswer()
+        {
+            return _answer;
         }
     }
 
